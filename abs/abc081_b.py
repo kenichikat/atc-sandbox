f4d4b00b028517adebcb54a4035e73e0c4345753
Kenichi KATO <k3nichikato@gmail.com>
2023-06-22 20:42:29 +0900

    first commit

---
+++ abs/abc081_b.py
@@ -0,0 +1,18 @@
+def main():
+    n = int(input())
+    a = list(map(int, input().split()))
+
+    cnt = 0
+    while True:
+        for i in range(n):
+            if a[i] % 2 == 0:
+                # a[i] = a[i] // 2
+                pass
+            else:
+                print(cnt)
+                exit()
+        cnt += 1
+
+
+if __name__ == '__main__':
+    main()
---
+++ abs/practice_1.py
@@ -0,0 +1,10 @@
+def main():
+    a = input()
+    b, c = input().split()
+    s = input()
+
+    print(int(a)+int(b)+int(c), s)
+
+
+if __name__ == '__main__':
+    main()
